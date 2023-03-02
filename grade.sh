da129df2a78dc52523c557c370129bd7b71d0915
kayden-yan <122576038+kayden-yan@users.noreply.github.com>
2023-03-02 15:03:45 -0800

    update

--- grade.sh
+++ grade.sh
@@ -26,16 +26,27 @@ cp TestListExamples.java test-field
 ## Complie
 set +e
 cd test-field
-javac -cp $CPATH *.java > compile-result.txt
-found=$(wc -l compile-result.txt)
+javac -cp $CPATH *.java > compile_result.txt
+found=$(wc -l compile_result.txt)
 if [[ $found > 0 ]]; then
     echo "Compile Error!"
-    echo compile-result.txt
+    echo compile_result.txt
     exit
 else
     echo "Compile Success!"
 fi
 
 ## Run Test
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-result.txt
-grep -o "Tests run: [0-9,]*," junit-result.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit_result.txt
+if grep -q "OK" junit_result.txt; then
+  echo "All tests passed!"
+  echo "Score: 100 %"
+else
+    grep -o "Tests run: [0-9,]*," junit_result.txt > test_run.txt
+    num_test=$(sed -e 's/.*run: \(.*\),.*/\1/' test_run.txt)
+    failure_str=$(grep -o "Failures: [0-9,]*" junit_result.txt)
+    num_fail=$(echo $failure_str | tr -dc '0-9')
+    echo "Test_run: " $num_test
+    echo "Failures: " $num_fail
+    echo "Score: " $((($num_test - $num_fail) / $num_test * 100)) "%"
+fi
